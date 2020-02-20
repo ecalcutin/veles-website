b8c2dc25a597c20ca3a873264aa7508146bd6650
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2020-02-20 23:49:32 +0300

    move navigation separate component

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar app class="veles-container">
+    <v-img
+      alt="Велес логотип"
+      class="shrink mr-2"
+      :src="require('@/assets/grave.svg')"
+      transition="scale-transition"
+      width="30"
+    />
+    <span style="font-size: 30px">𝐕𝐄𝐋𝐄𝐒</span>
+    <v-spacer></v-spacer>
+    <v-btn class="nav-button" :ripple="false" text to="/" tag="a">Главная</v-btn>
+    <v-btn class="nav-button" :ripple="false" text to="/gravestones" tag="a">Памятники</v-btn>
+    <v-btn class="nav-button" :ripple="false" text to="/accessories" tag="a">Атрибутика</v-btn>
+    <v-btn class="nav-button" :ripple="false" text to="/decor" tag="a">Декор</v-btn>
+    <v-btn class="nav-button" :ripple="false" text to="/services" tag="a">Услуги</v-btn>
+    <v-btn class="nav-button" :ripple="false" text to="/contacts" tag="a">КОнтакты</v-btn>
+  </v-app-bar>
+</template>
