28dcd4499ee2d8cc4e1eca3f482706aafd76c3be
ecalcutin <>
2020-02-21 11:47:17 +0200

    add footer

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
     <v-content>
       <router-view />
     </v-content>
+    <Footer />
   </v-app>
 </template>
 
 <script>
 import NavigationBar from "@/components/NavigationBar";
+import Footer from "@/components/Footer";
 export default {
   name: "App",
   components: {
-    NavigationBar
+    NavigationBar,
+    Footer
   },
   data: () => ({})
 };
---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-footer color="accent" padless>
+    <v-row justify="center" no-gutters>
+      <div class="py-4">
+        <v-btn color="white" text rounded>Главная</v-btn>
+        <v-btn color="white" text rounded>Памятники</v-btn>
+        <v-btn color="white" text rounded>Атрибутика</v-btn>
+        <v-btn color="white" text rounded>Декор</v-btn>
+        <v-btn color="white" text rounded>Услуги</v-btn>
+        <v-btn color="white" text rounded>Контакты</v-btn>
+      </div>
+      <v-col class="accent lighten-2 py-4 text-center white--text" cols="12">
+        <span>
+          2003 - {{currentYear}}
+          <strong>VELES S.R.L.</strong>
+        </span>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  computed: {
+    currentYear() {
+      return new Date().getFullYear();
+    }
+  }
+};
+</script>
