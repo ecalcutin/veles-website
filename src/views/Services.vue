61efde32717a5a69412ebf772826fb18f42b5fe6
ecalcutin <>
2020-02-21 15:04:14 +0200

    rename component

---
+++ src/views/Services.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <HeroHeading
+      title="Услуги"
+      description="Быстрое и квалифицированное решение организационных вопросов"
+    />
+  </div>
+</template>
+<script>
+import HeroHeading from "@/components/HeroHeading";
+export default {
+  components: {
+    HeroHeading
+  }
+};
+</script>
+<style scoped>
+</style>
