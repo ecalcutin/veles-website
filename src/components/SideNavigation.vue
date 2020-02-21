c641e69e10497c792210d3c59cbbbe3a12a4e9d4
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2020-02-22 00:34:39 +0300

    update sidenav

--- src/components/SideNavigation.vue
+++ src/components/SideNavigation.vue
@@ -1,6 +1,28 @@
 <template>
-  <v-navigation-drawer clipped v-model="sideNav" app>
-    <!-- -->
+  <v-navigation-drawer v-model="sideNav" app>
+    <v-list>
+      <v-subheader>Меню</v-subheader>
+      <v-list-item-group>
+        <v-list-item to="/">
+          <v-list-item-title>Главная</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/gravestones">
+          <v-list-item-title>Памятники</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/accessories">
+          <v-list-item-title>Атрибутика</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/decor">
+          <v-list-item-title>Декор</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/services">
+          <v-list-item-title>Услуги</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/contacts">
+          <v-list-item-title>Контакты</v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
