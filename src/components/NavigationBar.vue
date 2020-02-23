3d021cd5fb7e3e6313180146e520d20da3fa4e9b
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2020-02-23 14:37:22 +0300

    fix toolbar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,30 +1,30 @@
 <template>
-  <v-app-bar clipped-left app>
+  <v-app-bar clipped-left app id="toolbar">
     <div class="d-flex app-container">
-    <div id="brand-container" class="d-flex">
-      <v-app-bar-nav-icon @click="toggleSideNav" class="d-xs-flex d-md-none"></v-app-bar-nav-icon>
-      <v-spacer class="d-md-none d-sm-flex" />
-      <div class="d-flex align-center">
-        <v-img
-          alt="Велес логотип"
-          class="shrink mr-2"
-          :src="require('@/assets/images/logo.svg')"
-          transition="scale-transition"
-          width="30"
-          height="30"
-        />
-        <span>𝐕𝐄𝐋𝐄𝐒</span>
+      <div id="brand-container" class="d-flex">
+        <v-app-bar-nav-icon @click="toggleSideNav" class="d-xs-flex d-md-none"></v-app-bar-nav-icon>
+        <v-spacer class="d-md-none d-sm-flex" />
+        <div class="d-flex align-center">
+          <v-img
+            alt="Велес логотип"
+            class="shrink mr-2"
+            :src="require('@/assets/images/logo.svg')"
+            transition="scale-transition"
+            width="30"
+            height="30"
+          />
+          <span>𝐕𝐄𝐋𝐄𝐒</span>
+        </div>
+        <v-spacer class="d-none d-sm-flex"></v-spacer>
       </div>
-      <v-spacer class="d-none d-sm-flex"></v-spacer>
-    </div>
-    <v-toolbar-items class="d-none d-md-flex">
-      <v-btn text to="/" tag="a">Главная</v-btn>
-      <v-btn text to="/gravestones" tag="a">Памятники</v-btn>
-      <v-btn text to="/accessories" tag="a">Атрибутика</v-btn>
-      <v-btn text to="/decor" tag="a">Декор</v-btn>
-      <v-btn text to="/services" tag="a">Услуги</v-btn>
-      <v-btn text to="/contacts" tag="a">КОнтакты</v-btn>
-    </v-toolbar-items>
+      <v-toolbar-items class="d-none d-md-flex">
+        <v-btn text to="/" tag="a">Главная</v-btn>
+        <v-btn text to="/gravestones" tag="a">Памятники</v-btn>
+        <v-btn text to="/accessories" tag="a">Атрибутика</v-btn>
+        <v-btn text to="/decor" tag="a">Декор</v-btn>
+        <v-btn text to="/services" tag="a">Услуги</v-btn>
+        <v-btn text to="/contacts" tag="a">КОнтакты</v-btn>
+      </v-toolbar-items>
     </div>
   </v-app-bar>
 </template>
@@ -42,6 +42,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#toolbar {
+  .app-container {
+    height: 100%;
+  }
+}
 #brand-container {
   width: 100%;
   div {
