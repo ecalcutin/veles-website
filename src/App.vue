76e6cc494f689f57ed913c6e4087f24a8828f855
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2020-02-23 04:51:11 +0300

    add transition

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,15 @@
     <SideNavigation />
     <NavigationBar />
     <v-content>
-      <router-view />
+      <transition
+        name="fade"
+        mode="out-in"
+        @beforeLeave="beforeLeave"
+        @enter="enter"
+        @afterEnter="afterEnter"
+      >
+        <router-view />
+      </transition>
     </v-content>
     <Footer />
   </v-app>
@@ -20,9 +28,38 @@ export default {
     SideNavigation,
     Footer
   },
-  data: () => ({})
+  data: () => ({}),
+  methods: {
+    beforeLeave(element) {
+      this.prevHeight = getComputedStyle(element).height;
+    },
+    enter(element) {
+      const { height } = getComputedStyle(element);
+
+      element.style.height = this.prevHeight;
+
+      setTimeout(() => {
+        element.style.height = height;
+      });
+    },
+    afterEnter(element) {
+      element.style.height = "auto";
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: height, opacity;
+  transition-timing-function: ease;
+  overflow: hidden;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
 </style> 
