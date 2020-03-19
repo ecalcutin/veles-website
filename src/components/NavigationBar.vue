590644154d2ec2be0db8ec84050a8816dfa27ecd
ecalcutin <>
2020-03-19 12:15:12 +0200

    adjusted nav

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,9 +1,8 @@
 <template>
   <v-app-bar clipped-left app id="toolbar">
     <div class="d-flex app-container">
-      <div id="brand-container" class="d-flex">
+      <router-link to="/" tag="a" id="brand-container" class="d-flex">
         <v-app-bar-nav-icon @click="toggleSideNav" class="d-xs-flex d-md-none"></v-app-bar-nav-icon>
-        <v-spacer class="d-md-none d-sm-flex" />
         <div class="d-flex align-center">
           <v-img
             alt="Велес логотип"
@@ -16,14 +15,63 @@
           <span>𝐕𝐄𝐋𝐄𝐒</span>
         </div>
         <v-spacer class="d-none d-sm-flex"></v-spacer>
-      </div>
+      </router-link>
       <v-toolbar-items class="d-none d-md-flex">
-        <v-btn text to="/" tag="a">Главная</v-btn>
-        <v-btn text to="/gravestones" tag="a">Памятники</v-btn>
-        <!-- <v-btn text exact to="/products?category=Атрибутика" tag="a">Атрибутика</v-btn>
-        <v-btn text to="/decor" tag="a">Декор</v-btn>-->
+        <v-menu offset-y>
+          <template v-slot:activator="{on}">
+            <v-btn text v-on="on">Каталог</v-btn>
+          </template>
+          <v-card tile style="min-width: 250px">
+            <v-subheader>Меню</v-subheader>
+            <v-list tile>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-icon>mdi-grave-stone</v-icon>
+                </v-list-item-action>
+                <v-list-item-title>Памятники</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-icon>mdi-fireplace-off</v-icon>
+                </v-list-item-action>
+                <v-list-item-title>Варианты установок</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-icon>mdi-brush</v-icon>
+                </v-list-item-action>
+                <v-list-item-title>Художественное оформление</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-icon>mdi-face-recognition</v-icon>
+                </v-list-item-action>
+                <v-list-item-title>Фотокерамика</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-icon>mdi-christianity</v-icon>
+                </v-list-item-action>
+                <v-list-item-title>Ритуальная аттрибутика</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-icon>mdi-equalizer-outline</v-icon>
+                </v-list-item-action>
+                <v-list-item-title>Изделия из Ж/Бетона</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-icon>mdi-equalizer</v-icon>
+                </v-list-item-action>
+                <v-list-item-title>Изделия из гранита и мрамора</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-card>
+        </v-menu>
         <v-btn text to="/services" tag="a">Услуги</v-btn>
-        <v-btn text to="/contacts" tag="a">КОнтакты</v-btn>
+        <v-btn text to="/news" tag="a">Новости</v-btn>
+        <v-btn text to="/contacts" tag="a">Контакты</v-btn>
       </v-toolbar-items>
     </div>
   </v-app-bar>
@@ -49,5 +97,8 @@ export default {
       font-size: 30px;
     }
   }
+  text-decoration: none;
+  color: inherit;
+  cursor: pointer;
 }
 </style>
